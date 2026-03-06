65811c29d63719d7553dc5d2810a6b668de8ad80
unknown <thkim42750@gmail.com>
2026-03-06 11:46:45 +0900

    chore: initiate dotnet pjt

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,28 +1,23 @@
+﻿using System.Text;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
 using System.Windows.Input;
-using DesktopTodoCalendar.ViewModels;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
 
-namespace DesktopTodoCalendar.Views
+namespace DesktopTodo;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
 {
-    public partial class MainWindow : Window
+    public MainWindow()
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-            
-            // XAML에서 바인딩할 DataContext를 코드에서도 설정할 수 있습니다.
-            // 여기서는 뷰모델을 생성해서 연결해줍니다.
-            this.DataContext = new MainViewModel();
-        }
-
-        // 위젯 특성상 상단 타이틀바(Title bar)가 없기 때문에,
-        // 창의 아무 곳이나 잡고 드래그해서 위치를 옮길 수 있도록 해주는 필수 이벤트입니다.
-        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-        {
-            if (e.ChangedButton == MouseButton.Left)
-            {
-                this.DragMove();
-            }
-        }
+        InitializeComponent();
     }
 }
